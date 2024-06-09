f4fa3f436a02665c40a9ce3873c3c020c5448141
Paul Jovenn Sarmiento <pauljovenn.sarmiento@g.msuiit.edu.ph>
2024-06-09 15:19:36 +0800

    test commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 
+//test comments for route controller
 Route::get('/', [IndexController::class, 'welcome']);
 
 Route::get('/firstpage', [IndexController::class, 'firstpage']);
